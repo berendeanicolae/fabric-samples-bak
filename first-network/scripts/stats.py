100123c83634bb6f40ba1730e50f6a4b607e57cf
Berendea Nicolae <berendeanicolae@gmail.com>
2018-07-07 20:42:38 +0000

    extract network stats

--- first-network/scripts/stats.py
+++ first-network/scripts/stats.py
@@ -13,37 +13,46 @@ srvcName = sys.argv[1]
 
 def getContainerPid(service):
 	# node ID
-    cmd = "docker service ps -f '{{{{.Node}}}}' --no-resolve {srvc}".format(srvc=service)
+	cmd = "docker service ps --format '{{{{.Node}}}}' --no-resolve {srvc}".format(srvc=service)
 	p = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE)
 	srvcId, _ = p.communicate()
 
 	# node IP
-    cmd = "docker node inspect -f '{{{{.Status.Addr}}}}' {srvc}".format(srvc=srvcId.strip())
+	cmd = "docker node inspect --format '{{{{.Status.Addr}}}}' {srvc}".format(srvc=srvcId.strip())
 	p = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE)
 	srvcIp, _ = p.communicate()
 
 	# container ID
-    cmd = "ssh ubuntu@{ip} \"docker container ls -f '{{{{.ID}}}} {{{{.Names}}}}'\" | grep {srvc}".format(ip=srvcIp.strip(), srvc=service)
+	cmd = "ssh ubuntu@{ip} \"docker container ls --format '{{{{.ID}}}} {{{{.Names}}}}'\" | grep {srvc}".format(ip=srvcIp.strip(), srvc=service)
 	p = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE)
 	cnts, _ = p.communicate()
 	cnts = cnts.strip()
 	cntId, _ = cnts.split()
 
     # container PID
-    cmd = "ssh ubuntu@{ip} \"docker inspect -f {{{{.State.Pid}}}} {cnt}\"".format(cnt=cntId)
-    p = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE)
-    cntPid, _ = p.communicate()
+	cmd = "ssh ubuntu@{ip} \"docker inspect --format {{{{.State.Pid}}}} {cnt}\"".format(ip=srvcIp.strip(), cnt=cntId)
+	p = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE)
+	cntPid, _ = p.communicate()
 
-	return srvcIp, cntPid
+	return srvcIp.strip(), cntPid.strip()
 
 def getStats(srvcIp, cntPid):
 	while True:
-        cmd = "ssh ubuntu@{ip} \"cat /proc/{cnt}/net/dev\"".format(ip=srvcIp, cnt=cndPid)
+		cmd = "ssh ubuntu@{ip} \"cat /proc/{cnt}/net/dev\"".format(ip=srvcIp, cnt=cntPid)
 		p = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE)
 		netIo, _ = p.communicate()
 
-        print(netIo)
+		netIn = 0
+		netOut = 0
+		for net in netIo.splitlines()[2:]:
+			net = net.strip()
+			if len(net) == 0: continue
+
+			netIn += int(net.split()[1])
+			netOut += int(net.split()[9])
+		print("{}\t{}".format(netIn, netOut))
+		time.sleep(10)
 
 srvcIp, cntPid = getContainerPid(srvcName)
-
+getStats(srvcIp, cntPid)
 
